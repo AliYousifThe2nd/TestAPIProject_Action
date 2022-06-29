bf5e7df470f7ec653d971ae1728a9dce8e50f804
AliYousifThe2nd <alooy3d@gmail.com>
2022-06-29 13:04:53 +0300

    Trigger Build

--- deploy-scripts/application_start.sh
+++ deploy-scripts/application_start.sh
@@ -1,3 +1,3 @@
-dotnet publish /home/ec2-user/TestAPIProject -o /var/www/TestAPI
+sudo dotnet publish /home/ec2-user/TestAPIProject -o /var/www/TestAPI
 systemctl enable website.service
 systemctl start website.service
